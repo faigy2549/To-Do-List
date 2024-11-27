37851f2d2fccd8c509bb9092ce569e2aeb93aa7f
faigygit <faigy2549@gmail.com>
2024-11-27 02:47:07 +0200

    added git ignore and reverted package json

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f80af599b6030915dae8249fd3763d12204216c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3d5d90fc14e75cb35aa6fc4f92d42bfc515927")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
